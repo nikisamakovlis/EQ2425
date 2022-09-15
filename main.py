scaling and rotation done + fixed threshold done

main.py
@@ -7,8 +7,9 @@ def kp_detector_sift(img, n_features=0, contrast_threshold=0.185, edge_threshold
     kp, des = sift.detectAndCompute(img, None)  # keypoints and descriptors
     #print(len(kp))
     kp_img = cv2.drawKeypoints(img, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
     point_kp = cv2.KeyPoint_convert(kp)
-    return kp, point_kp, kp_img
+    return kp, point_kp, des, kp_img
 
 
 def kp_detector_surf(img, hessian_threshold=7000):
@@ -29,88 +30,93 @@ def kp_detector_general(images, tested_keypoint_detector):
     kp_list = []
     points_list = []
     kd_list = []
+    des_list = []
     print(len(images))
     for img in images:
         if tested_keypoint_detector == 'surf':
-            kp, points, _, kd = kp_detector_surf(img)
+            kp, points, des, kd = kp_detector_surf(img)
         else:
-            kp, points, kd = kp_detector_sift(img)
+            kp, points, des, kd = kp_detector_sift(img)
         kp_list.append(kp)
         points_list.append(points)
+        des_list.append(des)
         kd_list.append(kd)
     print(len(points_list))
 
-    return kp_list, kd_list, points_list
+    return kp_list, kd_list, des_list, points_list
 
 
-def calc_repeatability(rotated_points_list):
-    repeatability_list = [0] * len(rotated_points_list[1:])
-    original_points = rotated_points_list[0]
-    for ind, rotated_img_points in enumerate(rotated_points_list[1:]):  # for each rotated image
-        print(f'Searching in the {ind}-th rotated image ...')
+def calc_repeatability(transformed_points_list, original_points):
+    print(len(original_points))
+    repeatability_list = [0] * len(transformed_points_list)
+    #original_points = rotated_points_list[0]
+    for ind, transformed_img_points in enumerate(transformed_points_list):  # for each rotated image
+        print(f'Searching in the {ind}-th transformed image ...')
         for ind_kp, original_point in enumerate(original_points):
             # print(f'Matching with the {ind_kp}-th keypoint from the original image ...')
             x_original, y_original = original_point
-            for ind_kp_rotated, rotated_point in enumerate(rotated_img_points):
-                # new_origin_x = math.sqrt(img_height**2+img_width**2) / 2 * math.cos(math.radians(45+15*(ind_kp_rotated+1))) \
-                #                + img_height * math.cos(math.radians(90-15*(ind_kp_rotated+1)))
-                # new_origin_y = img_width * math.cos(math.radians(90-15*(ind_kp_rotated+1)))
-                # x_rotated, y_rotated = rotate_point((new_origin_x, new_origin_y), rotated_point, math.radians(-15*(ind_kp_rotated+1)))
-                x_rotated, y_rotated = rotated_point
-                if np.abs(x_original - x_rotated) <= 2 and np.abs(y_original - y_rotated) <= 2:
+            for ind_kp_rotated, transformed_point in enumerate(transformed_img_points):
+                x_transformed, y_transformed = transformed_point
+                if np.abs(x_original - x_transformed) <= 2 and np.abs(y_original - y_transformed) <= 2:
                     repeatability_list[ind] += 1
                     print(
-                        f'Matched keypoints detected ! - {ind_kp_rotated}-th in the rotated image matched with the {ind_kp}-th keypoint in the original image ...')
+                        f'Matched keypoints detected ! - {ind_kp_rotated}-th in the transformed image matched with the {ind_kp}-th keypoint in the original image ...')
                     break
     print(repeatability_list)
-    # surf: [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 6, 0, 0, 0, 0, 0, 402]
-    # sift: [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 0, 0, 2, 0, 0, 403]
     repeatability_list = [i / len(original_points) for i in repeatability_list]
     print(repeatability_list)
-    # surf: [0.0024875621890547263, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0024875621890547263, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0024875621890547263, 0.0, 0.0, 0.0, 0.014925373134328358, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
-    # sift: [0.0024813895781637717, 0.0, 0.0, 0.0, 0.0, 0.0024813895781637717, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.004962779156327543, 0.007444168734491315, 0.0, 0.0, 0.004962779156327543, 0.0, 0.0, 1.0]
     return repeatability_list
 
 
 def main():
     images = read_images('data1')
     # part2-a
-    #kp_sift, points, kp_img = kp_detector_sift(images[0])
-    #kp_surf, points, des_surf, img_surf = kp_detector_surf(images[0])
+    tested_keypoint_detector = 'surf'
+    if tested_keypoint_detector == 'sift':
+        kp_sift, points, des_sift, kp_img = kp_detector_sift(images[0])
+    else:
+        kp_surf, points, des_surf, img_surf = kp_detector_surf(images[0])
 
-    # part2-b
-    tested_keypoint_detector = 'sift'
-    rotated_imgs, inverted_matrices = rotate_image(images[0])
-    rotated_kp_list, rotated_kd_list, rotated_points_list = kp_detector_general(rotated_imgs, tested_keypoint_detector)
-
-    transformed_points_list = []
-    for idx, points_list in enumerate(rotated_points_list):
-        points_list = np.array(points_list)
-        transformed_points = transform_rotation(inverted_matrices[idx], points_list)
-        transformed_points_list.append(transformed_points)
-
-    # repeatability_list = calc_repeatability(transformed_points, images[0].shape[:2][-1], images[0].shape[:2][-2])
-    repeatability_list = calc_repeatability(transformed_points_list)
-    #plot_images(columns=4, rows=6, imgs=rotated_kd_list, tested_keypoint_detector=tested_keypoint_detector, augmentation='rotated', figsize=(40, 60))
+    # # part2-b
+    # rotated_imgs, inverted_matrices = rotate_image(images[0])
+    # rotated_kp_list, rotated_kd_list, rotated_points_list = kp_detector_general(rotated_imgs, tested_keypoint_detector)
+    #
+    # transformed_points_list = []
+    # for idx, points_list in enumerate(rotated_points_list):
+    #     points_list = np.array(points_list)
+    #     transformed_points = transform_rotation(inverted_matrices[idx], points_list)
+    #     transformed_points_list.append(transformed_points)
+    #
+    # print(transformed_points_list[0])
+    # print(points)
+    #
+    # repeatability_list = calc_repeatability(transformed_points_list, points)
 
     # # part 2-c
-    # scaled_images = scale_image(images[0])
-    # scaled_kp_list, scaled_kd_list, scaled_points_list = kp_detector_general(scaled_images, tested_keypoint_detector)
-    # #scaled_repeatability_list = calc_repeatability(scaled_points_list)
-    # plot_images(columns=3, rows=3, imgs=scaled_kd_list, tested_keypoint_detector=tested_keypoint_detector, augmentation='scaled', figsize=(60, 60))
+    scaled_images = scale_image(images[0])
+    scaled_kp_list, scaled_kd_list, scaled_des_list, scaled_points_list = kp_detector_general(scaled_images, tested_keypoint_detector)
 
-    # print(rotate_point((0,0), (1,1), math.radians(-45)))
+    # print(scaled_points_list[1])
+    transformed_scaled_points_list = []
+    for idx, points_list in enumerate(scaled_points_list):
+        scaled_points_each_img_list = []
+        for point in points_list:
+            x, y = point[0], point[1]
+            new_x = x/(1.2**idx)
+            new_y = y/(1.2**idx)
+            scaled_points_each_img_list.append([new_x, new_y])
+        transformed_scaled_points_list.append(scaled_points_each_img_list)
+    # print(transformed_scaled_points_list[1])
+    print(len(transformed_scaled_points_list))
 
-    # print(rotate_point((0, 0), (0,1), math.radians(-90)))
+    scaled_repeatability_list = calc_repeatability(transformed_scaled_points_list, points)
+    plot_repeatability('scaled', scaled_repeatability_list)
 
-
-
-
-
-
-
-
-
+    # # plot_images(columns=3, rows=3, imgs=scaled_kd_list, tested_keypoint_detector=tested_keypoint_detector, augmentation='scaled', figsize=(60, 60))
+    #
+    # # print(rotate_point((0,0), (1,1), math.radians(-45)))
+    #
+    # # print(rotate_point((0, 0), (0,1), math.radians(-90)))
 
 if __name__ == '__main__':
     main()

utils.py
@@ -22,7 +22,7 @@ def rotate_image(image):
     (h, w) = image.shape[:2]
     (cX, cY) = (w//2, h//2)  # finding the center point
     d = 0  # degree
-    for i in range(int(360/15)+1):
+    for d in range(0, 360 + 15, 15):
         rotation_matrix = cv2.getRotationMatrix2D((cX, cY), d, 1.0)
         inverted_matrix = cv2.getRotationMatrix2D((cX, cY), -d, 1.0)
         abs_cos, abs_sin = abs(rotation_matrix[0, 0]), abs(rotation_matrix[0,1])
@@ -40,7 +40,6 @@ def rotate_image(image):
 
         rotated_images.append(rotated_img)
         inverted_matrices.append(inverted_matrix)
-        d += 15
 
     return rotated_images, inverted_matrices
 
@@ -71,9 +70,9 @@ def scale_image(image):
     m = 1.2
     for i in range(0, 9):
         scaled_image = cv2.resize(image, None, fx=m**i, fy=m**i)
-        print(scaled_image.shape)
         scaled_images.append(scaled_image)
     return scaled_images
+
 
 
 def plot_images(columns, rows, imgs, tested_keypoint_detector, augmentation, figsize):
@@ -95,3 +94,30 @@ def plot_images(columns, rows, imgs, tested_keypoint_detector, augmentation, fig
             save_path = 'Scaled_images_sift1.png'
 
     plt.savefig(save_path, dpi=300)
+
+
+def plot_repeatability(augmentation, rep_list):
+    if augmentation is 'rotated':
+        x = np.arange(0, 360, 15)
+        title = 'Repeatability vs Rotation'
+        xlegend = 'Rotation'
+        ylegend = 'Repeatability'
+    else:
+        m = 1.2
+        x = [m**i for i in range(0, 9)]
+        title = 'Repeatability vs Scaling'
+        xlegend = 'Scaling'
+        ylegend = 'Repeatability'
+
+    plt.plot(x, rep_list)
+    plt.title(title)
+    plt.xlabel(xlegend)
+    plt.ylabel(ylegend)
+    plt.show()
+
+
+
+
+
+
+